5938be77a01b68edac95e69ba42dc6ad236951f9
seankudryk <seankudryk@hotmail.ca>
2025-05-16 17:15:19 -0400

    buildTree function finally works, and displays a visualation of the BST when tree.root is passed to prettyPrint function

--- script.js
+++ script.js
@@ -18,56 +18,75 @@ class Tree {
         });
 
         this.filteredArray = filteredArray;
+        this.start = 0;
+        this.end = filteredArray.length - 1;
         
-        this.root = this.buildTree(filteredArray);
+        this.root = this.buildTree(filteredArray, this.start, this.end);
     };
 
-    buildTree = (array) => {
-        console.log(array);
-        let first = 0;
-        let last = array.length - 1;
-        let midPoint = Math.floor(last / 2);
+    buildTree = (array, start, end) => {
+        if (start > end) {
+            return null;
+        }
+
+        let midPoint = start + Math.floor((end - start) / 2);
 
         let root = new Node(array[midPoint]);
-        root.left = this.buildLeft(array, first, midPoint - 1);
-        root.right = this.buildRight(array, midPoint + 1, last);
-        
-        console.log(root);
+        console.log(node.left);
+
+        root.left = this.buildTree(array, start, midPoint - 1);
+
+        root.right = this.buildTree(array, midPoint + 1, end);
 
         return root;
-    }
-
-    buildLeft = (array, first, last) => {
-        if (first > last) {
-            return null;
-        }
-        console.log(`Left Side: first = ${first}, last = ${last}`);
-
-        let midPoint = first + Math.floor((last - first) / 2);
-        let root = new Node(array[midPoint]);
-
-        root.left = this.buildLeft(array, first, midPoint - 1);
-    }
-
-    buildRight = (array, first, last) => {
-        if (first > last) {
-            return null;
-        }
-        console.log(`Right Side: first = ${first}, last = ${last}`);
-
-        let midPoint = first + Math.floor((last - first) / 2);
-        let root = new Node(array[midPoint]);
-
-        root.right = this.buildRight(array, midPoint + 1, last);
-    }
-
-    printFilteredInput = () => {
-        console.log(this.filteredArray);
-    } 
-
-    printRoot = () => {
-        console.log(this.root);
     };
+
+    // buildTree = (array) => {
+    //     console.log(array);
+    //     let first = 0;
+    //     let last = array.length - 1;
+    //     let midPoint = Math.floor(last / 2);
+
+    //     let root = new Node(array[midPoint]);
+    //     root.left = this.buildLeft(array, first, midPoint - 1);
+    //     root.right = this.buildRight(array, midPoint + 1, last);
+        
+    //     console.log(root);
+
+    //     return root;
+    // }
+
+    // buildLeft = (array, first, last) => {
+    //     if (first > last) {
+    //         return null;
+    //     }
+    //     console.log(`Left Side: first = ${first}, last = ${last}`);
+
+    //     let midPoint = first + Math.floor((last - first) / 2);
+    //     let root = new Node(array[midPoint]);
+
+    //     root.left = this.buildLeft(array, first, midPoint - 1);
+    // }
+
+    // buildRight = (array, first, last) => {
+    //     if (first > last) {
+    //         return null;
+    //     }
+    //     console.log(`Right Side: first = ${first}, last = ${last}`);
+
+    //     let midPoint = first + Math.floor((last - first) / 2);
+    //     let root = new Node(array[midPoint]);
+
+    //     root.right = this.buildRight(array, midPoint + 1, last);
+    // }
+
+    // printFilteredInput = () => {
+    //     console.log(this.filteredArray);
+    // } 
+
+    // printRoot = () => {
+    //     console.log(this.root);
+    // };
 };
 
 const tree = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
@@ -84,6 +103,7 @@ const prettyPrint = (node, prefix = "", isLeft = true) => {
     if (node.left !== null) {
       prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
     }
-  };
- 
-console.log(tree.root.left);
+};
+
+prettyPrint(tree.root);
+
